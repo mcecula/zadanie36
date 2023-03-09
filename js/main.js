9ce13d24b6052e678b579269c63e63313369b52e
mcecula <mc23051988@gmail.com>
2023-03-09 14:20:17 +0100

    zadanie36

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+function suma(firstNumber, secondNumber) {
+    if (firstNumber == 20 || secondNumber == 20 || firstNumber + secondNumber <= 20) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+
+}
+suma(10, 10);
